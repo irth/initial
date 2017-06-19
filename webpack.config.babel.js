228d9d01ecadf84f1e29f3ddf1e74a482573ddf7
Wojciech Kwolek <me@irth.pl>
2017-06-19 22:01:42 +0200

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const dir = (...args) => path.resolve(__dirname, ...args);
+const codedir = (...args) => dir('app', ...args);
+const outdir = (...args) => dir('dist', ...args);
+
+export default {
+  entry: codedir('index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: outdir(),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: codedir('index.html.ejs') })],
+  resolve: { extensions: ['.js', '.jsx'] },
+};
